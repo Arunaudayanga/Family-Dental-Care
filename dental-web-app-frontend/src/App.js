106ceefbd87ee7fa7efd3cfe0b7a5d4cc49be068
Ashen-Kavinda <89352601+Ashen-Kavinda@users.noreply.github.com>
2022-05-08 15:14:14 +0530

    payment frontend create

--- dental-web-app-frontend/src/App.js
+++ dental-web-app-frontend/src/App.js
@@ -3,7 +3,16 @@ import './App.css';
 import EditProfile from './views/Editprofile/EditProfile';
 import AddDoctor from './views/AddDoctor/AddDoctor';
 import DoctorSignin from './views/DoctorSignin/DoctorSignin';
+<<<<<<< Updated upstream
 import AdminPanel from "./views/AdminPanel/adminPanel";
+=======
+import AdminPanel from './views/AdminPanel/AdminPanel';
+import AdminSignin from './views/AdminSignin/AdminSignin';
+import Payment from './views/Payment/Payment';
+import AddPayment from './views/Payment/AddPayment';
+import EditPayment from './views/Payment/EditPayment';
+import Admin from "./views/AdminPanel/AdminPanel";
+>>>>>>> Stashed changes
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import DoctorProfile from './views/DoctorProfile/DoctorProfile';
 
@@ -27,6 +36,27 @@ function App() {
             <Route path='/AdminPanel' element={<AdminPanel/>} />
           </Routes>
           
+          <Routes>
+            <Route path='/AdminPanel' element={<AdminPanel/>} />
+          </Routes>
+          
+         
+          <Routes>
+            <Route path='/AdminSignin' element={<AdminSignin/>} />
+          </Routes>
+          
+          <Routes>
+            <Route path='/Payment' element={<Payment/>} />
+          </Routes>
+         
+          <Routes>
+            <Route path='/AddPayment' element={<AddPayment/>} />
+          </Routes>
+
+          <Routes>
+            <Route path='/EditPayment/:id' element={<EditPayment/>} />
+          </Routes>
+         
          
       </Router>
 
